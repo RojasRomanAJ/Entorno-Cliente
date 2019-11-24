d06c71b32cde9e23af848cd4251e66903e3ed27f
Antonio Jesús <antoniojesusr9@gmail.com>
2019-11-24 22:53:23 +0100

    ADD
    He añadido los ficheros de trabajo del ejercicio validacion de crear usuario

---
+++ validacionCrearUsuario/validarformulario.js
@@ -0,0 +1,150 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let inputNombre = document.getElementById("name");
+    inputNombre.addEventListener("keyup",validarNombre);
+
+    let inputApellido = document.getElementById("surname");
+    inputApellido.addEventListener("keyup",validarApellidos);
+
+    let inputEdad = document.getElementById("age");
+    inputEdad.addEventListener("keyup", validarEdad);
+
+    let inputTerminos = document.getElementById("politics");
+    inputTerminos.addEventListener("click", comprobarTerminos);
+
+    let formulario = document.getElementById("form");
+    formulario.addEventListener("submit", comprobarFormulario);
+});
+
+function validarNombre(event){
+    let esCorrecto = true;
+    let expresion = /^[a-z A-Z]{4,}$/;
+    let inputNombre = document.getElementById("name");
+    let valor = inputNombre.value.trim();
+    let listaErrores = document.getElementById("erroresNombre");
+    listaErrores.innerHTML = "";
+    inputNombre.classList.remove("inputErroneo");
+    inputNombre.classList.remove("inputCorrecto");
+
+    if(valor === ""){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "EL CAMPO NO DEBE ESTAR VACÍO";
+        listaErrores.appendChild(divError);
+    }
+
+    if(expresion.test(valor)){
+        esCorrecto = true;
+        inputNombre.classList.add("inputCorrecto");
+
+    }
+    
+    if(!expresion.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "SOLO DEBEN USARSE LETRAS Y DEBE CONTENER MINIMO 4 LETRAS";
+        listaErrores.appendChild(divError);
+        inputNombre.classList.add("inputErroneo");
+        
+    }
+    return esCorrecto;
+}
+
+function validarApellidos(event){
+    let esCorrecto = true;
+    let expresion = /^[a-z A-Z]{4,}$/;
+    let inputApellido = document.getElementById("surname");
+    let valor = inputApellido.value.trim();
+    let listaErrores = document.getElementById("erroresApellidos");
+    listaErrores.innerHTML = "";
+    inputApellido.classList.remove("inputErroneo");
+    inputApellido.classList.remove("inputCorrecto");
+
+    if(valor === ""){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "EL CAMPO NO DEBE ESTAR VACÍO";
+        listaErrores.appendChild(divError);
+    }
+
+    if(expresion.test(valor)){
+        esCorrecto = true;
+        inputApellido.classList.add("inputCorrecto");
+
+    }
+    
+    if(!expresion.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "SOLO DEBEN USARSE LETRAS Y DEBE CONTENER MINIMO 4 LETRAS";
+        listaErrores.appendChild(divError);
+        inputApellido.classList.add("inputErroneo");
+    }
+
+    return esCorrecto;
+}
+
+function validarEdad(event){
+    let esCorrecto = true;
+    let inputEdad = document.getElementById("age");
+    let valor = inputEdad.value.trim();
+    let listaErrores = document.getElementById("erroresEdad");
+    listaErrores.innerHTML = "";
+    inputEdad.classList.remove("inputErroneo");
+    inputEdad.classList.remove("inputCorrecto");
+
+    if (!/^[0-9]+$/.test(valor)) {
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "SOLO DEBE CONTENER NUMEROS";
+        listaErrores.appendChild(divError);
+        inputEdad.classList.add("inputErroneo");
+        
+    }else if(valor < 18){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "LA EDAD DEBE SER MAYOR DE 18";
+        listaErrores.appendChild(divError);
+        inputEdad.classList.add("inputErroneo");
+
+    } else {
+        inputEdad.classList.add("inputCorrecto");
+    }
+
+    return esCorrecto;
+}
+
+function comprobarTerminos(event){
+    let esCorrecto = false;
+    let inputTerminos = document.getElementById("politics");
+    let valor = inputTerminos.value;
+    let listaErrores = document.getElementById("errorCheck");
+    listaErrores.innerHTML = "";
+    inputTerminos.classList.remove("inputErroneo");
+    inputTerminos.classList.remove("inputCorrecto");
+
+    if(inputTerminos.checked){
+        inputTerminos.classList.add("inputCorrecto");
+        esCorrecto = true;
+    }else {
+        alert("DEBE ACEPTAR LOS TERMINOS");
+        inputTerminos.classList.add("inputErroneo");
+    }
+    return esCorrecto;
+}
+
+function comprobarFormulario(event){
+    event.preventDefault();
+    let esNombreCorrecto = validarNombre(document.getElementById("name"));
+    let esApellidoCorrecto = validarApellidos(document.getElementById("surname"));
+    let esEdadCorrecta = validarEdad(document.getElementById("age"));
+    let sonTerminosCorrectos = comprobarTerminos(document.getElementById("politics"));
+
+    if(esNombreCorrecto && esApellidoCorrecto && esEdadCorrecta && sonTerminosCorrectos){
+        let formulario = document.getElementById("form");
+        formulario.submit();
+        alert("EL USUARIO SE HA CREADO CORRECTAMENTE");
+    }else {
+        alert("ERROR EN EL FORMULARIO");
+    }
+}
