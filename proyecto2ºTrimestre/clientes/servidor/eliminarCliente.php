2acccf074cecb996ddfdc5b9a9da968584ff4e67
mabreon <45296926+mabreon@users.noreply.github.com>
2020-02-10 20:16:06 +0100

    UPD & ADD
    añadido la funcion de eliminar usuarios

---
+++ proyecto2ºTrimestre/clientes/servidor/eliminarCliente.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$baseDatos = "todomuebles";
+header('Content-Type: application/json; charset=utf-8');
+header('Cache-Control: no-cache, must-revalidate');
+ //Creamos la conexion
+$conexion = new mysqli($servidor, $usuario, $password, $baseDatos);
+$conexion->set_charset("utf8");
+$sql = "DELETE FROM Clientes where id=".$_POST["id"];
+$conexion->query($sql);
+echo json_encode(1);
