f9f013480e4a8c5b522214957f067e5fa8fa9933
Antonio Jesús <antoniojesusr9@gmail.com>
2020-02-17 19:37:12 +0100

    UPD
    He actualizado el codigo del fichero

--- proyecto2ºTrimestre/muebles/servidor/datosMueble.php
+++ proyecto2ºTrimestre/muebles/servidor/datosMueble.php
@@ -7,5 +7,5 @@ $baseDatos = "todomuebles";
 // Creamos la conexion
 $conexion = new mysqli($servidor, $usuario, $password,$baseDatos);
 $conexion->set_charset("utf8");
-$sql = "SELECT `id`, `nombre`, `tipo`, `tamanio`, `descripcion`, `Precio` FROM `muebles`";
+$sql = "SELECT `id`, `nombre`, `tipo`, `tamanio`, `descripcion`, `precio` FROM `muebles`";
 $muebles = $conexion->query($sql);
