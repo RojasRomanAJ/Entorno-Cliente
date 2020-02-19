74698c4c7e73fa236e695dc1bcf2e2389fb0f9e4
mabreon <45296926+mabreon@users.noreply.github.com>
2020-02-19 21:03:21 +0100

    ADD
    añadido el readme

--- proyecto2ºTrimestre/clientes/servidor/datosMYSQL.php
+++ proyecto2ºTrimestre/clientes/servidor/datosMYSQL.php
@@ -3,7 +3,7 @@ $servidor = "localhost";
 $usuario = "root";
 $password = "";
 $baseDatos = "todomuebles";
-
+ 
 // Creamos la conexion
 $conexion = new mysqli($servidor, $usuario, $password,$baseDatos);
 $conexion->set_charset("utf8");
--- proyecto2ºTrimestre/editarCliente.php
+++ proyecto2ºTrimestre/editarCliente.php
@@ -25,16 +25,16 @@
                 Búsquedas
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="busquedaProductos.html">Mostrar Productos</a>
+              <a class="dropdown-item" href="busquedaProductos.html">Mostrar Productos</a>
                 <a class="dropdown-item" href="busquedaClientes.html">Mostrar Clientes</a>
               </div>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 Administración Productos
+                Administración Productos
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="mostrarProductos.html">Mostrar Productos</a>
+                <a class="dropdown-item" href="mostrarProductos.html">Mostrar Producto</a>
                 <a class="dropdown-item" href="crearProducto.html">Crear Producto</a>
                 <a class="dropdown-item" href="editarProducto.html">Editar Producto</a>
                 <a class="dropdown-item" href="eliminarProducto.php">Eliminar Producto</a>
@@ -55,25 +55,58 @@
         </div>
       </nav>
       
-
-      
-
-
-
-
-
-
-
-
-
-
-
+<?php require_once 'clientes/servidor/datosMYSQL.php';  
+?>
+   <table class="table">
+     <thead>
+       <tr>
+         <th scope="col">Nombre</th>
+         <th scope="col">Apellidos</th>
+         <th scope="col">Producto</th>
+         <th scope='col'>Acciones</th>
+       </tr>
+       </thead>
+     <tbody>
+       <?php
+           while($cliente = $clientes->fetch_assoc()) {
+               ?>
+               <tr data-idCliente="<?php echo $cliente["id"]; ?>">
+                   <td><?php echo $cliente["nombre"]; ?></td>
+                   <td><?php echo $cliente["apellidos"]; ?></td>
+                   <td><?php echo $cliente["producto"]; ?></td>
+                   <td><button data-idEliminar="<?php echo $cliente["id"]; ?>" data-accion="eliminar">Eliminar</button></td>
+               </tr>    
+               <?php
+           }
+       ?>
+   </tbody>
+   </table>
+   <div id="modalEliminar" class="modal" tabindex="-1" role="dialog">
+     <div class="modal-dialog" role="document">
+       <div class="modal-content">
+         <div class="modal-header">
+           <h5 class="modal-title">Eliminar Cliente</h5>
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+             <span aria-hidden="true">&times;</span>
+           </button>
+         </div>
+         <div class="modal-body">
+           <p>¿Estás seguro de eliminar este Cliente?</p>
+         </div>
+         <div class="modal-footer">
+           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+           <button id="botonConfirmarEliminar" type="button"  class="btn btn-primary" data-accion="confirmar-eliminar" data-ideliminar="">Confirmar</button>
+         </div>
+       </div>
+     </div>
+   </div>
 
 
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
 integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
 crossorigin="anonymous"></script>
+<script src="clientes/js/eliminar.js" defer></script>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
 integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
@@ -82,6 +115,7 @@ crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
 integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
 crossorigin="anonymous"></script>
+<link rel="stylesheet" href="clientes/css/style.css">
 
 </body>
 </html>
